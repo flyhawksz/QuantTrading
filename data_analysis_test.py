cadf0eb075c6040522a8c21e796ba7a77cc21a4e
flyhawksz <flyhawksz@163.com>
2017-07-27 23:46:31 +0800

    change timeout

--- data_analysis_test.py
+++ data_analysis_test.py
@@ -4,16 +4,15 @@ import json
 
 def startMain():
     url = 'https://www.okcoin.cn/api/v1/ticker.do?symbol=btc_cny'
-    response = urllib.request.urlopen(url, timeout=3)  # 打开连接，timeout为请求超时时间
+    response = urllib.request.urlopen(url, timeout=5)  # 打开连接，timeout为请求超时时间
     data = response.read().decode('utf-8')  # 返回结果解码
     json_data = json.loads(data)
     print(data, type(data))
     print(json_data)
     print(json_data['ticker']['buy'])
 
-
     url = 'https://www.bitstamp.net/api/ticker/'
-    response = urllib.request.urlopen(url)
+    response = urllib.request.urlopen(url, timeout=5)  # 打开连接，timeout为请求超时时间
     data = response.read().decode('utf-8')  # 返回结果解码
     json_data = json.loads(data)
 
